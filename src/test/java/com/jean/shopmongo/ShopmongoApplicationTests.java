fb00aae83dbe8e06d12bb67e0cdc68552c4c161a
jcheavy <jcheavy@hotmail.com>
2021-02-13 17:28:02 -0300

    first commit

---
+++ src/test/java/com/jean/shopmongo/ShopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.shopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
